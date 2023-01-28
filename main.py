2b4c76738aa75dacb79c02663078a41847dd9126
Kirill Aleksandrovich <KArtamonov1@yandex.ru>
2023-01-28 21:41:23 +0300

    Add Doctrine

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from graphical_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """This function about inflicted damage from player."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -17,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """This function about block hits or not."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """This function talk about applied special skils player or not."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 f'применил специальное умение «Выносливость {80 + 25}»')
@@ -40,6 +43,8 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """This function, worked choise player such as training
+    and give players return result."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -64,6 +69,10 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """
+    This function give choise personal skin in
+    char_class, for player, and sucsessful choise.
+    Then"""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -89,6 +98,11 @@ def choice_char_class() -> str:
 
 if __name__ == '__main__':
     def main():
+        """This function Greetings people
+        and give choise path for continue game,
+        and question player he(she) name,
+        enter the name in input.
+        And sucsessful training players or not."""
         run_screensaver()
         print('Приветствую тебя, искатель приключений!')
         print('Прежде чем начать игру...')
